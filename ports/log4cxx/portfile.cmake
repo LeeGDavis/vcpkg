22cbf7c51f429b81b7caef91bc8770626afd50b0
Lee Davis <lee@leedavis.ca>
2020-03-08 22:39:58 -0700

    Update portfile.cmake
    Hash update for upstream changes

--- ports/log4cxx/portfile.cmake
+++ ports/log4cxx/portfile.cmake
@@ -3,7 +3,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO stephen-webb/logging-log4cxx
     REF master
-    SHA512 2f0a9afcc67974b4fa5e8c344ef8233169ad4a94f7a205d7d9ba7ba452f97c092adfb704b9f3b60b056d7b9cdb5afd9014310bc957041c847449725e63c3e2a6
+    SHA512 18940652e5e06638d8c1c7a8997245bd0d7d943562650d9beeb2b09416500c86bf54340edb3544eaf4b7e9b3a610aa5f90e156fcb8effcb196df21a9eb4f2845
     HEAD_REF master
 )
 
